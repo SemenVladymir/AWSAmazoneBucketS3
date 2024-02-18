fcb0f9091089f5f9a523d01f1320994d75d99119
VladimirSemen2022 <107301979+VladimirSemen2022@users.noreply.github.com>
2024-02-18 11:14:20 +0200

    Добавьте файлы проекта.

---
+++ AmazonS3.cs
@@ -0,0 +1,132 @@
+﻿using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.S3.Transfer;
+
+namespace AWSAmazoneBucket
+{
+    internal class AmazonS3
+    {
+        private string accessKey = "AKIA6ODUZCHYMHVJ45PO";
+        private string secretKey = "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";
+        private static string bucketName = "semenvladbucket";
+        private static AmazonS3 _instance;
+        private static AmazonS3Client s3Client;
+
+        private AmazonS3()
+        {
+            BasicAWSCredentials credentials = new BasicAWSCredentials(accessKey, secretKey);
+            s3Client = new AmazonS3Client(credentials, Amazon.RegionEndpoint.EUNorth1);
+        }
+
+        public static AmazonS3 GetClient()
+        {
+            if (_instance == null)
+            {
+                _instance = new AmazonS3();
+            }
+            return _instance;
+        }
+
+        public static void UploadFileToBucket(string filePath, string fileName) 
+        {
+            try
+            {
+                // Upload the file to Amazon S3
+                TransferUtility fileTransferUtility = new TransferUtility(s3Client);
+                fileTransferUtility.Upload(filePath, bucketName, fileName);
+                Console.WriteLine("Upload completed!");
+
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine("Error encountered on server. Message:'{0}' when writing an object", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
+            }
+        }
+
+        public static async Task ReadFileFromBucket(string bucketName, string fileName)
+        {
+            try
+            {
+                GetObjectRequest getRequest = new GetObjectRequest
+                {
+                    BucketName = bucketName,
+                    Key = fileName
+                };
+                using (var getResponse = await s3Client.GetObjectAsync(getRequest))
+                using (var sr = new StreamReader(getResponse.ResponseStream))
+                {
+                    var getResponseString = await sr.ReadToEndAsync();
+                    Console.WriteLine(getResponseString);
+                }
+
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine("Error encountered on server. Message:'{0}' when writing an object", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
+            }
+        }
+
+        public static async Task ShowAllBuckets()
+        {
+            try
+            {
+                var list = await s3Client.ListBucketsAsync();
+            if (list.ContentLength > 0)
+            {
+
+                foreach (var bucket in list.Buckets)
+                {
+                    Console.WriteLine($"Bucket-{bucket.BucketName}\n");
+                    var filelist = s3Client.ListObjectsAsync(new ListObjectsRequest() { BucketName = bucket.BucketName });
+
+                    foreach (var file in filelist.Result.S3Objects)
+                    {
+                        Console.WriteLine($"File---{file.Key}\n");
+                    }
+                }
+            }
+            else
+                Console.WriteLine("The bucket is empty!");
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine("Error encountered on server. Message:'{0}' when writing an object", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
+            }
+        }
+
+        public static async Task DeleteFile(string bucketName, string fileName)
+        {
+            try 
+            {
+                DeleteObjectRequest deleteRequest = new DeleteObjectRequest()
+                {
+                    BucketName = bucketName,
+                    Key = fileName
+                };
+                await s3Client.DeleteObjectAsync(deleteRequest);
+            }
+            catch (AmazonS3Exception e)
+            {
+                Console.WriteLine("Error encountered on server. Message:'{0}' when writing an object", e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unknown encountered on server. Message:'{0}' when writing an object", e.Message);
+            }
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using Amazon.S3;
+using Amazon.S3.Transfer;
+using Amazon.Runtime;
+using Amazon.S3.Model;
+using AWSAmazoneBucket;
+using System;
+
+AmazonS3.GetClient();
+        string inp, fileName, bucketName;
+        do
+        {
+            Console.Clear();
+            Console.WriteLine("What do you want to do?\n\n");
+            Console.WriteLine("1. Read file in the Azure blob");
+            Console.WriteLine("2. Add/Upload file into the Azure blob");
+            Console.WriteLine("3. Show all buckets consist the Amazon S3");
+            Console.WriteLine("4. Delete file from the bucket in the Amazon S3\n");
+            Console.WriteLine("0. Exit from program\n");
+            inp = Console.ReadLine();
+            if (int.TryParse(inp, out int res))
+            {
+                switch (res)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.Write("Enter the name of the bucket where the file is exist\n");
+                        bucketName = Console.ReadLine();
+                        Console.Write("Enter the name of file which you want to read\n");
+                        fileName = Console.ReadLine();
+                        AmazonS3.ReadFileFromBucket(bucketName, fileName);
+                        Thread.Sleep(3000);
+                        break;
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("Enter the path to the file which you want to upload\n");
+                        string path = Console.ReadLine();
+                        Console.Write("Enter the name of file which you want to upload\n");
+                        fileName = Console.ReadLine();
+                        AmazonS3.UploadFileToBucket(path, fileName);
+                        Thread.Sleep(3000);
+                        break;
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("Enter the path to the file which you want to upload\n");
+                        await AmazonS3.ShowAllBuckets();
+                        Thread.Sleep(3000);
+                        break;
+                    case 4:
+                        Console.Clear();
+                        Console.Write("Enter the name of the bucket where the file is exist\n");
+                        bucketName = Console.ReadLine();
+                        Console.Write("Enter the name of file which you want to delete\n");
+                        fileName = Console.ReadLine();
+                        await AmazonS3.DeleteFile(bucketName, fileName);
+                        Thread.Sleep(3000);
+                        break;
+                    case 0:
+                        return;
+                }
+            }
+        } while (true);
